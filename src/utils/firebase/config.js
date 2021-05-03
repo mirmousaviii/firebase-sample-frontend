18258b132c8542720dc9de6ef2357fd878925251
M. Mirmousavi <mirmousavi.m@gmail.com>
2021-05-03 03:00:09 +0200

    Initialize Firebase

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -7,3 +7,5 @@ export const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+export default firebaseConfig;
---
+++ src/utils/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import firebaseConfig from './config';
+require('firebase/auth')
+require('firebase/firestore')
+
+// Initialize Firebase
+let app;
+if (!firebase.apps.length) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+export const auth = firebase.auth(app);
+export const db = firebase.firestore(app);
+// firebase.firestore.setLogLevel("debug");
