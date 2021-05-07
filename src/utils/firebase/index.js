82677d0f45b252920a160f5d7d1d6a8ad24fbd29
M. Mirmousavi <mirmousavi.m@gmail.com>
2021-05-07 04:29:04 +0200

    Fix import in firebase

--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -1,7 +1,7 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 import firebaseConfig from './config';
-require('firebase/auth')
-require('firebase/firestore')
+import 'firebase/auth';
+import 'firebase/firestore';
 
 // Initialize Firebase
 let app;
